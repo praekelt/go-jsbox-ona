6125fce3ad35d5e4aa538f5535197eb6eaaab211
justinvdm <justinvdm.0.1.0@gmail.com>
2014-10-02 12:45:34 +0200

    Basic validation for submission data

---
+++ src/errors.js
@@ -0,0 +1,16 @@
+var vumigo = require('vumigo_v02');
+var BaseError = vumigo.utils.BaseError;
+
+
+var OnaValidationError = BaseError.extend(function(self, message) {
+    /**class:OnaValidationError
+    Thrown for invalid api requests.
+
+    :param String message:
+        The reason for the validation error.
+    */
+    self.message = message;
+});
+
+
+exports.OnaValidationError = OnaValidationError;
